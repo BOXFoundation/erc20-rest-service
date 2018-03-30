ce93c5b3b72c87b337a071080cb612f97bff5e59
soulmachine <soulmachine@gmail.com>
2018-03-30 16:55:22 -0700

    Fixed swagger-ui.html not accessible error

--- src/main/java/fm/castbox/wallet/config/SwaggerConfig.java
+++ src/main/java/fm/castbox/wallet/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package fm.castbox.wallet.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,19 +14,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("CastBox wallet RESTful API")
-                .description("CastBox wallet RESTful API that supports Ethereum and all ERC20 tokens")
-                .build();
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo)
-                .select()
-                // see https://github.com/springfox/springfox/issues/631
-                .apis(Predicates.not(
-                        RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                .build();
-    }
+public class SwaggerConfig implements WebMvcConfigurer {
+
+  @Bean
+  public Docket api() {
+    ApiInfo apiInfo = new ApiInfoBuilder()
+        .title("CastBox wallet RESTful API")
+        .description("CastBox wallet RESTful API that supports Ethereum and all ERC20 tokens")
+        .build();
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo)
+        .select()
+        // see https://github.com/springfox/springfox/issues/631
+        .apis(Predicates.not(
+            RequestHandlerSelectors.basePackage("org.springframework.boot")))
+        .build();
+  }
+
+  // https://github.com/springfox/springfox/issues/2155#issuecomment-352431668
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
 }
